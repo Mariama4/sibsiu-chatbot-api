e83485df734ad2e15fe7de55ca86d9f685d44ca2
mariama4 <mariama4@mail.ru>
2022-09-05 23:59:08 +0700

    python com line app for transform pdf to png

--- test_pdf.js
+++ test_pdf.js
@@ -1,20 +1,32 @@
-import axios from 'axios';
-import { response } from 'express';
+import fs from 'fs';
 import https from 'https';
 
-// let a = https.get(
-//   'https://www.sibsiu.ru/files/raspisanie/asi/СОБРАНИЯ%20%20ПЕРВОКУРСНИКОВ%20%2001.09.2022.pdf'
-// );
+const download = async (url, path) => {
+  https
+    .get(url, (res) => {
+      const stream = fs.createWriteStream(path);
 
-// console.log(a);
+      res.pipe(stream);
 
-axios
-  .get(
-    'https://www.sibsiu.ru/files/raspisanie/asi/СОБРАНИЯ%20%20ПЕРВОКУРСНИКОВ%20%2001.09.2022.pdf'
-  )
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+      stream.on('finish', () => {
+        stream.close();
+        console.log('Image downloaded');
+      });
+    })
+    .on('error', (err) => {
+      // handle error
+      console.log(err);
+    });
+};
+
+let urls = [
+  'https://www.sibsiu.ru/files/raspisanie/asi/Расписание%20занятий/1%20курс%20Осенний%20семестр%202022-2023.pdf',
+  'https://www.sibsiu.ru/files/raspisanie/asi/Расписание%20занятий/2%20курс%20Осенний%20семестр%202022-2023.pdf',
+  'https://www.sibsiu.ru/files/raspisanie/asi/Расписание%20занятий/3%20курс%20Осенний%20семестр%202022-2023.pdf',
+  'https://www.sibsiu.ru/files/raspisanie/asi/Расписание%20занятий/4%20курс%20Осенний%20семестр%202022-2023.pdf',
+].map((el) => decodeURIComponent(el));
+
+// urls.forEach((url) => {
+//   const fileName = url.split('/').pop();
+//   download(url, `./img/${fileName}`);
+// });

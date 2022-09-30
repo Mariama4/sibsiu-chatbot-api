fa893917861c2f568182d7d3b8cb6d689cea7e65
mariama4 <mariama4@mail.ru>
2022-09-30 12:37:41 +0700

    upd

--- src/controllers/configuration.Controller.js
+++ src/controllers/configuration.Controller.js
@@ -21,6 +21,25 @@ class ConfigurationController {
     }
   }
 
+  async updateStatus(req, res, next) {
+    const { id, status } = req.body;
+
+    const configuration = await TelegramBotConfiguration.update(
+      {
+        status,
+      },
+      { where: { id } }
+    );
+    if (!Number(configuration)) {
+      return next(ApiError.internal('Запись с таким id не найдена.'));
+    } else {
+      const updatedConfiguration = await TelegramBotConfiguration.findOne({
+        where: { id },
+      });
+      return res.json({ message: 'Запись обновлена.', updatedConfiguration });
+    }
+  }
+
   async getData(req, res, next) {
     const configuration = await TelegramBotConfiguration.findAll();
     return res.json({ configuration });
--- src/routes/telegram_bot_configuration.Router.js
+++ src/routes/telegram_bot_configuration.Router.js
@@ -4,6 +4,7 @@ import ConfigurationController from '../controllers/configuration.Controller.js'
 const router = new Router();
 
 router.patch('/token', ConfigurationController.updateToken);
+router.patch('/status', ConfigurationController.updateStatus);
 router.get('/', ConfigurationController.getData);
 
 export default router;
